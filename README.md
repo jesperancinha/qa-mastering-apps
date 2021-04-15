d01b1db57a3e8817f04e67cca3a150ed69dd75b0
Joao Esperancinha <joao.esperancinha@eonics.nl>
2021-04-15 18:56:38 +0200

    Project foundations

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Car Lease
 
+## How to install
+
+```bash
+sdk install java 16.0.0.hs-adpt
+sdk use java 16.0.0.hs-adpt
+```
---
+++ car-lease-service/src/test/java/org/jesperancinha/car/lease/CarLeaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jesperancinha.car.lease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarLeaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
