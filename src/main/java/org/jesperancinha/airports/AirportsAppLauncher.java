fd3d413a28d4b47afbf2ccdddb2a727bcba05358
Joao Esperancinha <jofisaes@gmail.com>
2023-01-09 09:41:57 +0100

    Prepares SpringBoot code

---
+++ src/main/java/org/jesperancinha/airports/AirportsAppLauncher.java
@@ -0,0 +1,11 @@
+package org.jesperancinha.airports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportsAppLauncher {
+    public static void main(String[] args) {
+        SpringApplication.run(AirportsAppLauncher.class);
+    }
+}
