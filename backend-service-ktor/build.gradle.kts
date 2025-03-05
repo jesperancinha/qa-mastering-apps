b37e83d6ebf7ebe26d75d24f7b06235b894054f6
João Esperancinha <jofisaes@gmail.com>
2025-03-06 00:29:17 +0100

    Gradlew

--- backend-service-ktor/build.gradle.kts
+++ backend-service-ktor/build.gradle.kts
@@ -31,3 +31,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.12.0")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.1")
 }
+
+tasks.test {
+    useJUnitPlatform()
+}
--- backend-service-ktor/src/test/kotlin/org/jesperancinha/books/MainTest3.kt
+++ backend-service-ktor/src/test/kotlin/org/jesperancinha/books/MainTest3.kt
@@ -2,15 +2,16 @@ package org.jesperancinha.books
 
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import io.ktor.server.testing.*
 import io.ktor.http.*
-import io.ktor.server.application.*
-import io.ktor.server.routing.*
-import io.ktor.server.response.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.serialization.gson.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.testing.*
 import kotlinx.coroutines.delay
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.parallel.Execution
 import org.junit.jupiter.api.parallel.ExecutionMode
@@ -28,6 +29,7 @@ class ApplicationTest3 {
     @Test
     @Execution(ExecutionMode.CONCURRENT)
     fun testHelloEndpoint1() = testApplication {
+        println(Thread.currentThread())
         application(testApp())
         client.get("/hello").apply {
             assertEquals(HttpStatusCode.OK, status)
@@ -39,41 +41,57 @@ class ApplicationTest3 {
     @Test
     @Execution(ExecutionMode.CONCURRENT)
     fun testHelloEndpoint2() = testApplication {
+        println(Thread.currentThread())
         application(testApp())
         client.get("/hello").apply {
             assertEquals(HttpStatusCode.OK, status)
             assertEquals("Hello, Ktor!", bodyAsText())
             delay(5.seconds)
         }
+        println(Thread.currentThread())
     }
     @Test
     @Execution(ExecutionMode.CONCURRENT)
     fun testHelloEndpoint3() = testApplication {
+        println(Thread.currentThread())
         application(testApp())
         client.get("/hello").apply {
             assertEquals(HttpStatusCode.OK, status)
             assertEquals("Hello, Ktor!", bodyAsText())
             delay(5.seconds)
         }
+        println(Thread.currentThread())
     }
     @Test
     @Execution(ExecutionMode.CONCURRENT)
     fun testHelloEndpoint4() = testApplication {
+        println(Thread.currentThread())
         application(testApp())
         client.get("/hello").apply {
             assertEquals(HttpStatusCode.OK, status)
             assertEquals("Hello, Ktor!", bodyAsText())
             delay(5.seconds)
         }
+        println(Thread.currentThread())
     }
     @Test
     @Execution(ExecutionMode.CONCURRENT)
     fun testHelloEndpoint5() = testApplication {
+        println(Thread.currentThread())
         application(testApp())
         client.get("/hello").apply {
             assertEquals(HttpStatusCode.OK, status)
             assertEquals("Hello, Ktor!", bodyAsText())
             delay(5.seconds)
+        }
+        println(Thread.currentThread())
+    }
+
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun setup() {
+            println("All tests started")
         }
     }
 }
