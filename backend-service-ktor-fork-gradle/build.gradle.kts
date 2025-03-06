d38c1527c96adf228f1d7fb67b18c4b3d09b424d
João Esperancinha <jofisaes@gmail.com>
2025-03-06 09:39:53 +0100

    Makes test with forks for gradle

--- backend-service-ktor-fork-gradle/build.gradle.kts
+++ backend-service-ktor-fork-gradle/build.gradle.kts
@@ -10,12 +10,21 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation(platform("io.ktor:ktor-bom:3.1.1"))
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-serialization-gson-jvm")
+    testImplementation("io.ktor:ktor-server-test-host")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.12.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.12.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.1")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
 }
---
+++ backend-service-ktor-fork-gradle/src/test/kotlin/org/jesperancinha/books/MainTest3.kt
@@ -0,0 +1,90 @@
+package org.jesperancinha.books
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.serialization.gson.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.testing.*
+import kotlinx.coroutines.delay
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import kotlin.time.Duration.Companion.seconds
+
+class ApplicationTest3 {
+
+    private fun testApp(): Application.() -> Unit = {
+        install(ContentNegotiation) { gson() }
+        routing {
+            get("/hello") { call.respondText("Hello, it's me!") }
+        }
+    }
+
+    @Test
+    fun testHelloEndpoint1() = testApplication {
+        println(Thread.currentThread())
+        application(testApp())
+        client.get("/hello").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello, it's me!", bodyAsText())
+            delay(5.seconds)
+        }
+    }
+
+    @Test
+    fun testHelloEndpoint2() = testApplication {
+        println(Thread.currentThread())
+        application(testApp())
+        client.get("/hello").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello, it's me!", bodyAsText())
+            delay(5.seconds)
+        }
+        println(Thread.currentThread())
+    }
+    @Test
+    fun testHelloEndpoint3() = testApplication {
+        println(Thread.currentThread())
+        application(testApp())
+        client.get("/hello").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello, it's me!", bodyAsText())
+            delay(5.seconds)
+        }
+        println(Thread.currentThread())
+    }
+    @Test
+    fun testHelloEndpoint4() = testApplication {
+        println(Thread.currentThread())
+        application(testApp())
+        client.get("/hello").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello, it's me!", bodyAsText())
+            delay(5.seconds)
+        }
+        println(Thread.currentThread())
+    }
+    @Test
+    fun testHelloEndpoint5() = testApplication {
+        println(Thread.currentThread())
+        application(testApp())
+        client.get("/hello").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello, it's me!", bodyAsText())
+            delay(5.seconds)
+        }
+        println(Thread.currentThread())
+    }
+
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun setup() {
+            println("All tests started")
+        }
+    }
+}
