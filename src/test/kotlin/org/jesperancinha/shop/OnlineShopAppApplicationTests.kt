dfd291f2cc241443717fcc645656f84d56eede05
João Esperancinha <jofisaes@gmail.com>
2025-05-18 16:01:54 +0200

    OPS - Runs containerized

--- src/test/kotlin/org/jesperancinha/shop/OnlineShopAppApplicationTests.kt
+++ src/test/kotlin/org/jesperancinha/shop/OnlineShopAppApplicationTests.kt
@@ -1,13 +1,23 @@
 package org.jesperancinha.shop
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 
 @SpringBootTest
 class OnlineShopAppApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @MockitoBean
+    lateinit var elasticsearchClient: ElasticsearchClient
+
+    @MockitoBean
+    lateinit var elasticSearchTemplate: ElasticsearchTemplate
+
+    @Test
+    fun contextLoads() {
+    }
 
 }
