be64abc219019f9cf92aa2bfcc70ff3a5219a760
João Esperancinha <jofisaes@gmail.com>
2025-06-18 22:17:04 +0200

    Validation in Spring

--- supermarket/src/main/kotlin/org/jesperancinha/supermaket/controller/DeliveryController.kt
+++ supermarket/src/main/kotlin/org/jesperancinha/supermaket/controller/DeliveryController.kt
@@ -1,10 +1,12 @@
 package org.jesperancinha.supermaket.controller
 
+import jakarta.validation.Valid
 import org.jesperancinha.supermaket.dto.DeliveriesSummaryDto
 import org.jesperancinha.supermaket.dto.DeliveryRequestDto
 import org.jesperancinha.supermaket.dto.InvoiceRequestDto
 import org.jesperancinha.supermaket.service.DeliveryService
 import org.springframework.http.HttpStatus
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 
 /**
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.*
  * The Controller layer shouldn't be aware at all the domain model, because that it is not its concern.
  * In addition to that, making integration tests with a controller that contains domain data types is cumbersome and leads to bad results.
  */
+
+@Validated
 @RestController
 @RequestMapping("/deliveries")
 class DeliveryController(
@@ -21,7 +25,7 @@ class DeliveryController(
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun createDelivery(@RequestBody request: DeliveryRequestDto) = service.createDelivery(request)
+    fun createDelivery(@RequestBody @Valid request: DeliveryRequestDto) = service.createDelivery(request)
 
     @PostMapping("/invoice")
     fun getInvoices(@RequestBody request: InvoiceRequestDto) = service.getInvoicesForDeliveries(request)
--- supermarket/src/main/kotlin/org/jesperancinha/supermaket/dto/Dto.kt
+++ supermarket/src/main/kotlin/org/jesperancinha/supermaket/dto/Dto.kt
@@ -1,17 +1,25 @@
 package org.jesperancinha.supermaket.dto
 
+import jakarta.validation.Constraint
+import jakarta.validation.ConstraintValidator
+import jakarta.validation.ConstraintValidatorContext
+import jakarta.validation.Payload
+import jakarta.validation.constraints.PastOrPresent
 import org.jesperancinha.supermaket.domain.DeliveryStatus
 import java.time.Instant
 import java.util.*
+import kotlin.reflect.KClass
 
 // For a small project there is NO inconvenience in using DTOS in one single file.
 // If anyone says otherwise for a small project like this one, then please think of how that would look like.
 // It would look like Java code, but in Kotlin, which isn't what is intended with Kotlin files.
 // This is perfect and the way to declare Dtos in projects, if there are that many Dtos's in a project then they can be declared in separate files, where the concern makes sense.
 
+@DeliveryStatusConstraint
 data class DeliveryRequestDto(
     val vehicleId: String,
     val address: String,
+    @field:PastOrPresent
     val startedAt: Instant,
     val finishedAt: Instant?,
     val status: DeliveryStatus
@@ -50,3 +58,21 @@ data class DeliveriesSummaryDto(
     val deliveries: Int,
     val averageMinutesBetweenDeliveryStart: Long
 )
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@Constraint(validatedBy = [DeliveryStatusValidator::class])
+annotation class DeliveryStatusConstraint(
+    val message: String = "Invalid delivery status and finishedAt combination",
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Payload>> = []
+)
+
+class DeliveryStatusValidator : ConstraintValidator<DeliveryStatusConstraint, DeliveryRequestDto> {
+    override fun isValid(dto: DeliveryRequestDto, context: ConstraintValidatorContext): Boolean {
+        return when (dto.status) {
+            DeliveryStatus.IN_PROGRESS -> dto.finishedAt == null
+            DeliveryStatus.DELIVERED -> dto.finishedAt != null
+        }
+    }
+}
